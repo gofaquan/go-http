0da50b1116e8c212cfb27e1e67e32ccc05bc1e2c
gofaquan <2660796265@qq.com>
2022-12-17 17:42:25 +0800

    加入filter, 初步支持中间件

--- server.go
+++ server.go
@@ -1,5 +1,7 @@
 package main
 
+import "net/http"
+
 // Server 定义 http Server 的顶级抽象
 type Server interface {
 	Start(address string) error
@@ -10,4 +12,43 @@ type Server interface {
 type Routable interface {
 	// Route 设定一个路由，命中该路由的会执行 handleFunc 的代码
 	Route(method, pattern string, handleFunc HandleFunc)
+}
+
+// sdkHttpServer 这个是基于 net/http 这个包实现的 http server
+type sdkHttpServer struct {
+	// Name server 的名字，给个标记，日志输出的时候用得上
+	Name    string
+	handler Handler
+	root    Filter
+}
+
+func (s *sdkHttpServer) Route(method string, pattern string,
+	handlerFunc HandleFunc) {
+	s.handler.Route(method, pattern, handlerFunc)
+}
+
+func (s *sdkHttpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c := NewContext(w, r)
+	s.root(c)
+}
+func (s *sdkHttpServer) Start(address string) error {
+
+	return http.ListenAndServe(address, s)
+	//return http.ListenAndServe(address, s.handler)
+}
+
+func NewSdkHttpServer(name string, builders ...FilterBuilder) Server {
+	handler := NewTreeHandler()
+	var root Filter = handler.ServeHTTP
+	for i := len(builders) - 1; i >= 0; i-- {
+		b := builders[i]
+		root = b(root)
+	}
+
+	res := &sdkHttpServer{
+		Name:    name,
+		handler: handler,
+		root:    root,
+	}
+	return res
 }
